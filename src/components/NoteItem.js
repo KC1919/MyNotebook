2eddf8723f85c912c75ae67b979fd06c3bb4cb84
KC1919 <kunal.chandra1900@gmail.com>
2021-09-29 18:59:10 +0530

    notes layout structure

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -3,13 +3,18 @@ import React from "react";
 export default function NoteItem(props) {
   const { note } = props;
   return (
-    <div className="my-2 col-md-3">
-      <div className="card" style={{width:"18rem"}}>
+    <div className="col-md-3">
+      <div className="card my-3" style={{width:"18rem"}}>
         <div className="card-body">
           <h5 className="card-title">{note.title}</h5>
           <p className="card-text">
           {note.description}
           </p>
+          <div className="d-flex align-items-center">
+          <i className="far fa-trash-alt mx-2"></i>
+          <i className="far fa-edit mx-2"></i>
+          </div>
+          
         </div>
       </div>
     </div>
